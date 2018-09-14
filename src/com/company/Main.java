8be1b5ca80e5f941fd5a7f8aba22f9fbd3ff2846
ArmandasRokas <armandas.rokas@gmail.com>
2018-09-14 10:43:31 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -80,10 +80,7 @@ public class Main {
 
     public static void andenGradsLigning(){
         double a, b, c, diskriminant, x1, x2;
-// test
-        // test 2
-        // test 3
-        // test 4
+        
         Scanner s = new Scanner(System.in);
         System.out.print("Enter A B C med mellemrum: ");
         a = s.nextDouble();
